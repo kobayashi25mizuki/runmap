a51f1167251d48c5ffcf3a266fc3cf0ceb5a9d0c
MikzukiKobayashi <199931208+kobayashi25mizuki@users.noreply.github.com+mizukikobayashi@users.noreply.github.com>
2025-07-02 07:45:54 +0000

    検索機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   validates :name, presence: true
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
 end
