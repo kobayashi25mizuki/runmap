b28fb84bcf70e83222d8ffff8cd143b6dc665e72
MikzukiKobayashi <199931208+kobayashi25mizuki@users.noreply.github.com+mizukikobayashi@users.noreply.github.com>
2025-07-06 04:59:04 +0000

    Add Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :destroy, :edit, :update]
   get "home/about" => "homes#about", as: "about"
   get '/search', to: 'searches#search'
