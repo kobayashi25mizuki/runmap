f5925acdeea85a508d83fb35165ae9504822beb0
MikzukiKobayashi <199931208+kobayashi25mizuki@users.noreply.github.com+mizukikobayashi@users.noreply.github.com>
2025-07-10 14:59:36 +0000

    Postレイアウト完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,17 @@
 class Post < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 
   def self.search_for(content, method)
     if method == 'perfect'
