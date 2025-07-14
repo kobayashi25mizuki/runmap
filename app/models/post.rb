85207382294272b235d2cf7f10707942375318f2
MikzukiKobayashi <199931208+kobayashi25mizuki@users.noreply.github.com+mizukikobayashi@users.noreply.github.com>
2025-07-14 07:01:20 +0000

    いいね機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
 
@@ -23,5 +24,9 @@ class Post < ApplicationRecord
     else
       Post.where('title LIKE ?', '%'+content+'%')
     end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :destroy, :edit, :update]
